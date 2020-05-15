b40060ba10a9d62de52b4f77f92b35653c797f4c
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2020-05-14 17:04:18 -0700

    some adjustments

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,13 +4,9 @@ import React from "react"
 import AccordionData from "./AccordionData"
 import "./Home.css"
 import Links from "./Links"
-// colors=#080147 blue
-// colors=#fa7268 pinkish
-// colors=#6c757d grey
-// colors=#151515 black
 
-// logo="https://mistro.io/static/media/logo.68833b29.png"
-// background="https://mistro.io/static/media/background-white.768ea084.svg"
+
+
 let Home=()=>{
   return (
     <div style={{flexDirection:"column"}}>
